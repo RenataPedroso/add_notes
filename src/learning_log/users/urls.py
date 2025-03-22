896afb68f2c73e17f2adce3ffd8e013f6cf4bc2e
Renata Pedroso <renatanicollyy1@gmail.com>
2025-03-22 16:23:11 -0300

    Adicionando Logout, configurando rotas e redirecionamento para pagina inicial

--- src/learning_log/users/urls.py
+++ src/learning_log/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from . import views
 #importando uma view propria do django para login, que faz a verificacao de login e senha e autentica o user
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', views.logout_view , name='logout'),
 ]
